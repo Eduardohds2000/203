73b760259a77dd9f1cc4589dd72ce3cf5bbe8f97
nataliavalentin <nataliaferreiravalentin@gmail.com>
2019-06-12 16:39:32 -0300

    adicionar

---
+++ script.js
@@ -0,0 +1,101 @@
+// CONSTRUIR UMA ESCADA
+// O MATERIAL DA ESCADA PODE SER * # $
+// QUANTOS DEGRAUS ELE QUER
+
+// let material = prompt('digite o material da escada');
+// let degrau = material;
+// let qdt = prompt('Quantos degraus você quer');
+
+//     for(let i=0; i<qdt; i++){
+//       console.log(material);
+//         material = material + degrau;
+//     }
+
+// let filme = [
+//     {titulo:"Harry potter", idade:10, ator:"mateus"},
+//     {titulo: "it, a coisa", idade: 16},
+//     {titulo: "O albergue", idade : 18}
+// ]
+// console.log(filme[0].ator);
+
+// let filmes= [
+//     {titulos:['madagascar','frozen','divertidamente'], idade: 12},
+//     {titulos:[ 'deadpool','vingadores','xmen'], idade: 16},
+//     {titulos: ['american-pie','o albergue'],idade:18}
+// ];
+// let idadeUser = Number(prompt("Digite sua idade"));
+// for(i=0; i < filmes.length; i++ ){
+//     if( idadeUser >= filmes[i].idade){
+//     alert('Os filmes para a sua idade temos: '+ filmes[i].titulos);
+//     }
+// }
+
+
+
+
+
+
+
+// FUNÇÃO
+
+// function somar(x,y){
+//    console.log(x + y); // CONTEXTO 
+// }
+// somar(2, 3);
+// somar(5, 150);
+
+// let soma =Number( prompt("digite um número: "));
+// let soma2 = Number(prompt("digite um outro número: "));
+// function somar (soma, soma2){
+//     console.log(soma + soma2);
+// }
+  
+// somar(soma , soma2);
+
+
+
+
+// function calculadora (x ,y ){
+//     let soma = x + y;
+//     let mult = x * y;
+//     let div = x /y;
+//     let sub = x - y;
+//     console.log(`A media dela é : ${soma}`);
+//     console.log(`A media dela é : ${sub}`)
+//     console.log(`A media dela é : ${mult}`)
+//     console.log(`A media dela é : ${div}`)
+// }
+
+// calculadora(9,8);
+
+// calculadora = (a,b) => {console.log(a + b)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
